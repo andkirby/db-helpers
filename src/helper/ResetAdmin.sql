8f818ce4490762d795a71ab1cdc3244724468610
Andrew Roslik <andkirby@gmail.com>
2015-05-06 04:02:46 +0300

    - Added ResetAdmin procedure.

---
+++ src/helper/ResetAdmin.sql
@@ -0,0 +1,13 @@
+# Reset first admin user
+delimiter //
+DROP PROCEDURE IF EXISTS ResetAdmin//
+CREATE PROCEDURE ResetAdmin (admin_password VARCHAR(255))
+  BEGIN
+    -- Update password
+    UPDATE `admin_user` SET `username` = 'admin', `password` = md5(admin_password)
+      WHERE  `user_id` = 1;
+    -- Reset failures
+    UPDATE `admin_user` SET `failures_num` = 0, `is_active` = 1, `first_failure` = NULL, `lock_expires` = NULL
+      WHERE `user_id` = 1;
+  END//
+delimiter ;
